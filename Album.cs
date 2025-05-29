85daab163ead308e18a18593a451587bf29a3f2d
Mauro-Roncata <mauroroncata@gmail.com>
2025-05-29 15:10:32 -0300

    Add files via upload

---
+++ Album.cs
@@ -0,0 +1,29 @@
+﻿class Album
+{
+    private List<Musica> musicaList = new List<Musica>();
+    public string Nome { get;  }
+
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+
+    public int DuracaoTotal => musicaList.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicaList.Add(musica);
+    }
+
+    public void ExibirMusicaAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
+        foreach (var musica in musicaList)
+        {
+        Console.WriteLine($"Múscia: {musica.Nome}");
+        }
+        int minutosTotal = DuracaoTotal / 60;
+        int segundosTotal = DuracaoTotal % 60;
+        Console.WriteLine($"\nDuração total do album: {minutosTotal} minutos e {segundosTotal} segundos");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Banda queen = new Banda("Queen");
+
+Album albumQueen = new Album("A night at the opera");
+
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+
+
+albumQueen.AdicionarMusica(musica1);
+albumQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumQueen);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumQueen.ExibirMusicaAlbum();
+queen.ExibirDiscografia();
